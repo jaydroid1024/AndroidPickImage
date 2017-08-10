057bec34c484855b685ff58dc415d55f06c28987
jay.wang <jay.wang@imaginato.com>
2017-08-10 17:46:52 +0800

    add single pike picture

--- app/src/main/java/org/jay/androidpickimage/single/SinglePickActivity.java
+++ app/src/main/java/org/jay/androidpickimage/single/SinglePickActivity.java
@@ -82,6 +82,7 @@ public class SinglePickActivity extends AppCompatActivity {
         CropImage.startPickImageActivity(this);
     }
 
+    
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
